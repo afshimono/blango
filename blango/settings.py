5816f3f2bcf18556f830450839af7127e7285d35
Alexandre Shimono <ashimono@gmail.com>
2022-03-28 04:53:44 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -56,7 +56,8 @@ class Dev(Configuration):
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
-      "debug_toolbar"
+      "debug_toolbar",
+      'rest_framework'
   ]
 
   MIDDLEWARE = [
